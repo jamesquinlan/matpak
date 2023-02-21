c4015603dff3965984c85b2ca61fe9acd00fb4c3
jamesquinlan <jquinlan@une.edu>
2023-02-20 19:12:14 -0500

    kth standard basis element

---
+++ ek.m
@@ -0,0 +1,9 @@
+function [e] = ek(k,n)
+% ek = kth Elementary (or Standard) basis element
+% ek = (0, 0, 0, ... , 1, 0, 0, 0, ... ,0)'
+
+% ----------------------------- %
+e=zeros(n,1);e(k)=1;
+% ----------------------------- %
+end
+
