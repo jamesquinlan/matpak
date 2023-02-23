fcdb1a951b78144d96cd530701ff21bdccd300d9
jamesquinlan <jquinlan@une.edu>
2023-02-23 07:30:00 -0500

    kth standard basis element

--- ek.m
+++ ek.m
@@ -1,9 +1,19 @@
 function [e] = ek(k,n)
 % ek = kth Elementary (or Standard) basis element
 % ek = (0, 0, 0, ... , 1, 0, 0, 0, ... ,0)'
+% 
+% See also matpak
 
 % ----------------------------- %
-e=zeros(n,1);e(k)=1;
+if nargin < 2
+    n = k;
+end
+
+if k > n
+    k = n;
+end
+
+e = zeros(n,1); e(k)=1;
 % ----------------------------- %
 end
 
